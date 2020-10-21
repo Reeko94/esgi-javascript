ee4de41b7a8e08580eef8ca22f2465297fda0bc3
Théo SIKLI <theo@theo-sikli.fr>
2020-10-21 15:18:31 +0200

    capitalize function

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -2,3 +2,8 @@ function ucfirst(chaine) {
     if (typeof chaine !== "string" || chaine === "") return "";
     return chaine.charAt(0).toUpperCase() + chaine.slice(1);
 }
+
+function capitalize(chaine) {
+    if (typeof chaine !== "string" || chaine === "") return "";
+    return chaine.split(' ').map(c => c.charAt(0).toUpperCase() + c.slice(1)).join(' ');
+}
