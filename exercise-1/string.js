910922b6183667313e7ca5d8409b7efcb63c0fa5
Théo SIKLI <theo@theo-sikli.fr>
2020-10-22 00:11:34 +0200

    Update & reduce vig

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -48,32 +48,22 @@ function yoda(chaine) {
     return chaine.split(' ').reverse().join(' ');
 }
 
-
-function vig(string, code) {
-    if (typeof string !== "string") return "";
-    if (string.length === 0) return string;
-
-    while (code.length < string.length) {
+function vig(str, code) {
+    while (code.length < str.length) {
         code += code;
     }
-    code = code.substr(0, string.length);
     let codeIndex = 0;
+    return str.split("").map(function(car) {
+        car = car.toLowerCase();
+        const carCode = car.charCodeAt(0) - "a".charCodeAt(0);
 
-    return string
-        .split("")
-        .map((letter, index) => {
-            letter = letter.toLowerCase();
-            const aCode = "a".charCodeAt(0);
-            const letterNumber = letter.charCodeAt(0) - aCode; // [0-25]
+        if (carCode < 0 || carCode > 25) return car;
+        const codeCode = code[codeIndex++].charCodeAt(0) - "a".charCodeAt(0);
 
-            if (letterNumber < 0 || letterNumber > 25) return letter;
+        const encodedCode = (carCode + codeCode) % 26;
 
-            const codeNumber = code.charCodeAt(codeIndex) - aCode; // [0-25]
-            codeIndex++;
-
-            return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
-        })
-        .join("");
+        return String.fromCharCode(encodedCode + 'a'.charCodeAt(0));
+    }).join('');
 }
 
 function prop_access(obj, path) {
